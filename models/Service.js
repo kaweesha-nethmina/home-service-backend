e46c86a466afe2378a7d32e0f847f7931c6eab5e
Hiruni Chamathka <IT22205674@my.sliit.lk>
2025-08-31 11:44:14 +0530

    feat: Service Categories & Provider Listing epic

---
+++ models/Service.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const ServiceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    provider: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: false,
+        default: 'Colombo'
+    },
+    images: {
+        type: [String],
+        default: []
+    },
+    availability: {
+        type: Boolean,
+        default: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
